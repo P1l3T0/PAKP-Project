a33f85895fc702bbd2bf8c0cb9fca302a4e1354d
Vasil Ganev <vgan3v@gmail.com>
2025-11-15 12:17:06 +0200

    feat(proj): add database

---
+++ Backend/PAKPProjectAPI/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PAKPProjectData;
+
+namespace PAKPProjectAPI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterDTO registerDto)
+        {
+            return Ok("Register successful");
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDTO loginDto)
+        {
+            return Ok("Login successful");
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> GenerateNewRefreshToken()
+        {
+            return Ok("RefreshToken");
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return Ok("Logged out successfully");
+        }
+    }
+}
--- Backend/PAKPProjectAPI/Program.cs
+++ Backend/PAKPProjectAPI/Program.cs
@@ -1,9 +1,58 @@
+using PAKPProjectData;
+using System.Text;
 using Scalar.AspNetCore;
+using DotNetEnv;
+using DotNetEnv.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddDotNetEnv(".env", LoadOptions.TraversePath())
+    .Build();
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    string connectionString = builder.Configuration["Appsettings:DefaultConnection"]!;
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "https://localhost:7167/",
+            ValidAudience = "your-api-identifier",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("e3d6623891a3c57d8fec2ff34d9f7c91adb38a43a2b8ac7cf1d62b65a2f23c0f"))
+        };
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", options =>
+    {
+        string frontendUrl = builder.Configuration["Appsettings:FrontendURL"]!;
+
+        options.WithOrigins(frontendUrl)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -13,8 +62,9 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+app.UseCors("AllowSpecificOrigin");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
