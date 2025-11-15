130bd9e08c4dafa2f51d3ae37bf1a05997fa1450
Vasil Ganev <vgan3v@gmail.com>
2025-11-15 12:14:57 +0200

    feata(data): add models

---
+++ Backend/PAKPProjectData/Models/User/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PAKPProjectData
+{
+    public class RefreshToken : BaseModel
+    {
+        public int UserID { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public bool IsRevoked { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
+        [JsonIgnore]
+        public User? User { get; set; }
+    }
+}
---
+++ Backend/PAKPProjectData/Models/User/User.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace PAKPProjectData
+{
+    public class User : BaseModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required, MinLength(10), MaxLength(200)]
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+
+        [Required, MinLength(10), MaxLength(200)]
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+
+        [JsonIgnore]
+        public ICollection<RefreshToken> RefreshTokens { get; } = new List<RefreshToken>();
+
+        public override T ToDto<T>()
+        {
+            CurrentUserDTO currentUser = new CurrentUserDTO()
+            {
+                ID = ID,
+                Email = Email,
+                Username = Username,
+                PasswordHash = PasswordHash,
+                PasswordSalt = PasswordSalt,
+                DateCreated = DateCreated,
+            };
+
+            return (T)(object)currentUser;
+        }
+    }
+}
